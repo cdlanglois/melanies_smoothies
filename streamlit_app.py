31c0c85ee76cdea23495acc6c50c35b053e74170
cdlanglois <43684940+cdlanglois@users.noreply.github.com>
2026-03-16 10:34:04 -0400

    Update streamlit_app.py
    Expose the JSON Data Inside the Response Object
    fix bad URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,7 @@ if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
 
+# New section to display smoothiefroot nutrtition informantion
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+st.text(smoothiefroot_response.json)
